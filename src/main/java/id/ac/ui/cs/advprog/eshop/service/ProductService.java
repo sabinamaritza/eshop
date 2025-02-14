18b24c5490025cff4e0228930ea670a623b5492a
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2025-02-14 20:33:31 +0700

    add edit feature in product service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,4 +6,6 @@ import java.util.List;
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
+    Product findById(String id);
+    Product edit(Product product);
 }
