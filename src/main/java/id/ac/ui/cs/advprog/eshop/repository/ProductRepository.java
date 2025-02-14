9f077d14699ad88e88518a5378803a962dfe057f
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2025-02-14 20:32:51 +0700

    add edit feature in product repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,20 +3,50 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    private Map<String, Product> nameToProductMap = new HashMap<>();
+    private Map<String, Product> idToProductMap = new HashMap<>();
+    int idCounter = 0;
 
     public Product create(Product product) {
-        productData.add(product);
+        Product existingProduct = nameToProductMap.get(product.getProductName());
+
+        if (existingProduct != null) {
+            int currentQuantity = existingProduct.getProductQuantity() + product.getProductQuantity();
+            existingProduct.setProductQuantity(currentQuantity);
+            return existingProduct;
+        } else {
+            productData.add(product);
+            nameToProductMap.put(product.getProductName(), product);
+            product.setProductId(""+idCounter);
+            idToProductMap.put(product.getProductId(), product);
+            idCounter++;
+        }
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+
+    public Product edit(Product product) {
+        Product existingProduct = findById(product.getProductId());
+        System.out.println(product.getProductId());
+
+        if(existingProduct != null) {
+            existingProduct.setProductName(product.getProductName());
+            existingProduct.setProductQuantity(product.getProductQuantity());
+            return existingProduct;
+        }
+        return null;
+    }
+
+    public Product findById(String id) {
+        return idToProductMap.get(id);
+    }
+
 }
