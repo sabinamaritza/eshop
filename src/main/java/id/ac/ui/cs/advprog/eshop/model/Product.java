c7bb7c9422f9837a73e47758036665e6d314a3ed
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2026-02-04 17:07:43 +0700

    Add UUID and Product Constructor to Product model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,21 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter @Setter
 public class Product {
     private String productId;
     private String productName;
     private int productQuantity;
+
+    public Product() {
+        this.productId = UUID.randomUUID().toString();
+    }
+
+    public Product(String productId, String productName, int productQuantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productQuantity = productQuantity;
+    }
 }
