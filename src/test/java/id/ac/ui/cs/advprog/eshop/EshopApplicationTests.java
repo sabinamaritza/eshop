9c20f40106a38926194340b65bd3531f8adbf738
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2026-02-24 20:48:13 +0700

    create eshop application test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,11 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EshopApplicationTests {
+class EshopApplicationTest {
 
 	@Test
-	void contextLoads() {
+	void mainMethodRuns() {
+		EshopApplication.main(new String[] {});
 	}
-
 }
