1e0f43bd9b66a67d657008e4206bcef0e5ac8c89
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2026-03-03 19:10:12 +0700

    move uuid creation from car repository to model

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -2,20 +2,13 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public class CarRepository {
-    static int id = 0;
     private List<Car> carData = new ArrayList<>();
 
     public Car create(Car car) {
-        if (car.getCarId() == null) {
-            UUID uuid = UUID.randomUUID();
-            car.setCarId(uuid.toString());
-        }
         carData.add(car);
         return car;
     }
@@ -26,7 +19,7 @@ public class CarRepository {
 
     public Car findById(String id) {
         for (Car car : carData) {
-            if (car.getCarId().equals(id)) {
+            if (id.equals(car.getCarId())) {
                 return car;
             }
         }
