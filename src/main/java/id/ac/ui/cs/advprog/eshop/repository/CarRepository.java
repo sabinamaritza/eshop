41724a7e84804918012d20b320e23e61ffc6d38d
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2026-03-03 20:03:13 +0700

    separate car repository interface and implementation

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -1,45 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.repository;
+
 import id.ac.ui.cs.advprog.eshop.model.Car;
-import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
 import java.util.List;
 
-@Repository
-public class CarRepository {
-    private List<Car> carData = new ArrayList<>();
-
-    public Car create(Car car) {
-        carData.add(car);
-        return car;
-    }
-
-    public List<Car> findAll() {
-        return new ArrayList<>(carData);
-    }
-
-    public Car findById(String id) {
-        for (Car car : carData) {
-            if (id.equals(car.getCarId())) {
-                return car;
-            }
-        }
-        return null;
-    }
-
-    public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
-            if (car.getCarId().equals(id)) {
-                car.setCarName(updatedCar.getCarName());
-                car.setCarColor(updatedCar.getCarColor());
-                car.setCarQuantity(updatedCar.getCarQuantity());
-                return car;
-            }
-        }
-        return null;
-    }
-
-    public void delete(String id) {
-        carData.removeIf(car -> car.getCarId().equals(id));
-    }
+public interface CarRepository {
+    Car create(Car car);
+    List<Car> findAll();
+    Car findById(String id);
+    Car update(String id, Car updatedCar);
+    void delete(String id);
 }
