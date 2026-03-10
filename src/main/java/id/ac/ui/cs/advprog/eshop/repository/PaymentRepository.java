0260b088501355de78c9b38930e7327ef038f346
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2026-03-10 19:48:37 +0700

    [GREEN] implement Payment repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -3,11 +3,37 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class PaymentRepository {
-    public Payment save(Payment payment) { return null; }
-    public Payment findById(String id) { return null; }
-    public List<Payment> findAll() { return null; }
+    private List<Payment> paymentData = new ArrayList<>();
+
+    public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i++;
+        }
+        paymentData.add(payment);
+        return payment;
+    }
+
+    public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        return null;
+    }
+
+    public List<Payment> findAll() {
+        return new ArrayList<>(paymentData);
+    }
 }
