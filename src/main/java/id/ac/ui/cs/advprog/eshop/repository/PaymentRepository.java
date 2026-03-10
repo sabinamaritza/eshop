db47c83989d2e87242389cb7cf978059a840af5e
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2026-03-10 19:47:17 +0700

    [RED] add Payment repository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PaymentRepository {
+    public Payment save(Payment payment) { return null; }
+    public Payment findById(String id) { return null; }
+    public List<Payment> findAll() { return null; }
+}
